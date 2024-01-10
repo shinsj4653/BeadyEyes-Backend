4373eb5a00ac955c90b35360384a30147783617b
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-01-11 00:46:44 +0900

    feat: add testcontroller

--- src/main/java/gdsc/pointer/domain/User.java
+++ src/main/java/gdsc/pointer/domain/User.java
@@ -1,8 +1,18 @@
 package gdsc.pointer.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 
+@Entity
 @Getter
+@Table(name = "TB_USER")
 public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(length = 30)
+    private String name;
 
 }
