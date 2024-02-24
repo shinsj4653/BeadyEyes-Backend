9a40b0656842b70617af500598c2ccb6eb6ee828
SEONG JUN SHIN <shinsj4653@gmail.com>
2024-02-25 02:03:09 +0900

    style: clean up config code

--- src/main/java/gdsc/pointer/config/GCSConfig.java
+++ src/main/java/gdsc/pointer/config/GCSConfig.java
@@ -6,13 +6,13 @@ import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class GCSConfig {
+
     @Value("${spring.cloud.gcp.storage.project-id}")
     private String projectId;
 
